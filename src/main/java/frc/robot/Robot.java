67b5c903d8dee270880830d9fe02bdd41aabcfb4
Jakeahorner <jakeahorner@gmail.com>
2023-01-12 20:21:22 -0600

    i see cones

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -27,7 +28,10 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+
     m_robotContainer = new RobotContainer();
+    
   }
 
   /**
