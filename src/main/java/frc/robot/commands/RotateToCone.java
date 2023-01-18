2e9c5575b94c31c0afd5bea65a322ef45e2010c2
Jake Horner <67403499+Jakeahorner@users.noreply.github.com>
2023-01-17 19:49:29 -0600

    fix stuff (#4)
    Co-authored-by: Ryan Erickson <erirya29@gmail.com>

--- src/main/java/frc/robot/commands/RotateToCone.java
+++ src/main/java/frc/robot/commands/RotateToCone.java
@@ -72,7 +72,6 @@ public class RotateToCone extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    test
   }
 
   // Returns true when the command should end.
