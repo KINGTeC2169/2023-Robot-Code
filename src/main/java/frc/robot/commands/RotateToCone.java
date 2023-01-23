3c0f4994b8ea7ee7c6885a05c8f5b40a0016f0ab
Ryan Erickson <erirya29@gmail.com>
2023-01-23 17:02:32 -0600

    MAJOR L

--- src/main/java/frc/robot/commands/RotateToCone.java
+++ src/main/java/frc/robot/commands/RotateToCone.java
@@ -18,108 +18,109 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class RotateToCone extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Claw claw;
-  private final SwerveSubsystem swerve;
-  private final Arm arm;
+	@SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+	private final Claw claw;
+	private final SwerveSubsystem swerve;
+	private final Arm arm;
 
 
-  private final Timer time;
-  private double xSpeed;
-  private double ySpeed;
-  private double turningSpeed;
-  private double wristTurn;
-  private double angle;
-  private boolean isAngle;
-  private boolean readingAngle;
-  private boolean haveCone;
-  private boolean haveCube;
-  private ChassisSpeeds chassisSpeeds;
-  
-  private final PIDController pidTurn;
-  private final PIDController pidX;
-  private final PIDController pidY;
-  /**
-   * Creates a new ExampleCommand.
-   *
-   * @param subsystem The subsystem used by this command.
-   */
-  public RotateToCone(Claw claw, SwerveSubsystem swerve, Arm arm) {
-    this.claw = claw;
-    this.swerve = swerve;
-    this.arm = arm;
+	private final Timer time;
+	private double xSpeed;
+	private double ySpeed;
+	private double turningSpeed;
+	private double wristTurn;
+	private double angle;
+	private boolean isAngle;
+	private boolean readingAngle;
+	private boolean haveCone;
+	private boolean haveCube;
+	private ChassisSpeeds chassisSpeeds;
+	
+	private final PIDController pidTurn;
+	private final PIDController pidX;
+	private final PIDController pidY;
+	/**
+	 * Creates a new ExampleCommand.
+	 *
+	 * @param subsystem The subsystem used by this command.
+	 */
+	public RotateToCone(Claw claw, SwerveSubsystem swerve, Arm arm) {
+		this.claw = claw;
+		this.swerve = swerve;
+		this.arm = arm;
+		// Use addRequirements() here to declare subsystem dependencies.
+		addRequirements(claw);
+		addRequirements(swerve);
+		addRequirements(arm);
+		pidTurn = new PIDController(0.5, 0, 0);
+		pidX = new PIDController(0.5, 0, 0);
+		pidY = new PIDController(0.5, 0, 0);
+		time = new Timer();
+	}
 
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(claw);
-    addRequirements(swerve);
-    addRequirements(arm);
-    pidTurn = new PIDController(0.5, 0, 0);
-    pidX = new PIDController(0.5, 0, 0);
-    pidY = new PIDController(0.5, 0, 0);
-    time = new Timer();
-  }
+	// Called when the command is initially scheduled.
+	@Override
+	public void initialize() {
+		time.start();
+		claw.resetTwistEncoder();
+		angle = NetworkTables.getPalmAngle("Cone");
+	}
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    time.start();
-    claw.resetTwistEncoder();
-    angle = NetworkTables.getPalmAngle("Cone");
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    //angle = NetworkTables.getAngle();
-    if(!haveCone && !haveCube) {
-      if (NetworkTables.getPalmCenter("Cone")[0] != 0)
-        haveCone = true;
-      else if (NetworkTables.getPalmCenter("Cube")[0] != 0)
-        haveCube = true;
-    }
-
-
-    //  x/2048 * 360
-    if(haveCone) {
-      xSpeed = pidX.calculate(NetworkTables.getPalmCenter("Cone")[0], 0);//setpoint should be whatever the center of the image is
-      ySpeed = pidY.calculate(NetworkTables.getPalmCenter("Cone")[1], 0);
-      claw.twistClaw(pidTurn.calculate(NetworkTables.getPalmAngle("Cone"), 0));
-    }
-    else if(haveCube) {
-      xSpeed = pidX.calculate(NetworkTables.getPalmCenter("Cube")[0], 0);//setpoint should be whatever the center of the image is
-      ySpeed = pidY.calculate(NetworkTables.getPalmCenter("Cube")[1], 0);
-    }
-    else {
-      
-      turningSpeed = pidTurn.calculate(NetworkTables.getFrontCenter()[0], 0);// center of image
-    }
+	// Called every time the scheduler runs while the command is scheduled.
+	@Override
+	public void execute() {
+		//angle = NetworkTables.getAngle();
+		if(!haveCone && !haveCube) {
+		if (NetworkTables.getPalmCenter("Cone")[0] != 0)
+			haveCone = true;
+		else if (NetworkTables.getPalmCenter("Cube")[0] != 0)
+			haveCube = true;
+		}
 
 
-    chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turningSpeed);
-        
+		//  x/2048 * 360
+		if(haveCone) {
+			xSpeed = pidX.calculate(NetworkTables.getPalmCenter("Cone")[0], 0);//setpoint should be whatever the center of the image is
+			ySpeed = pidY.calculate(NetworkTables.getPalmCenter("Cone")[1], 0);
+			claw.twistClaw(pidTurn.calculate(NetworkTables.getPalmAngle("Cone"), 0));
+		}
+		else if(haveCube) {
+			xSpeed = pidX.calculate(NetworkTables.getPalmCenter("Cube")[0], 0);//setpoint should be whatever the center of the image is
+			ySpeed = pidY.calculate(NetworkTables.getPalmCenter("Cube")[1], 0);
+		}
+		else {
+			if(NetworkTables.closestObject() != null) {
+				turningSpeed = pidTurn.calculate(NetworkTables.getFrontCenter()[0], 0);// center of image
+				ySpeed = pidY.calculate(NetworkTables.getFrontCenter()[1], 0);
+			}
+		}
 
-    SwerveModuleState[] moduleStates = DriveConstants.DRIVE_KINEMATICS.toSwerveModuleStates(chassisSpeeds);
 
-    swerve.setModuleStates(moduleStates);
+		chassisSpeeds = new ChassisSpeeds(xSpeed, ySpeed, turningSpeed);
+			
+
+		SwerveModuleState[] moduleStates = DriveConstants.DRIVE_KINEMATICS.toSwerveModuleStates(chassisSpeeds);
+
+		swerve.setModuleStates(moduleStates);
 
 
-    //System.out.println("Current angle: " + currentAngle + "\tAngle: " + power + "\tPower: " + power);
+		//System.out.println("Current angle: " + currentAngle + "\tAngle: " + power + "\tPower: " + power);
 
 
-  }
+	}
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    haveCone = false;
-    haveCube = false;
-  }
+	// Called once the command ends or is interrupted.
+	@Override
+	public void end(boolean interrupted) {
+		haveCone = false;
+		haveCube = false;
+	}
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    //time.getUsClock()() > 1 && Math.abs(NetworkTable.getAngle()) < 5?
-    //return isAngle;
-    return false;
-  }
+	// Returns true when the command should end.
+	@Override
+	public boolean isFinished() {
+		//time.getUsClock()() > 1 && Math.abs(NetworkTable.getAngle()) < 5?
+		//return isAngle;
+		return false;
+	}
 }
