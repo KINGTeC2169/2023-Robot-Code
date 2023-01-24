376b0dd8d145c12364289c27b2e2e940c59be738
Ryan Erickson <erirya29@gmail.com>
2023-01-24 09:01:04 -0600

    oops :)

--- src/main/java/frc/robot/commands/GetCubone.java
+++ src/main/java/frc/robot/commands/GetCubone.java
@@ -106,8 +106,6 @@ public class GetCubone extends CommandBase {
 	// Called once the command ends or is interrupted.
 	@Override
 	public void end(boolean interrupted) {
-		haveCone = false;
-		haveCube = false;
 	}
 
 	// Returns true when the command should end.
