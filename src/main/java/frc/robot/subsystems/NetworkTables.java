6bc5781eb88fa6299c01f2aee45d068773b75a15
jakeahorner <jakeahorner@gmail.com>
2023-02-20 15:57:21 -0600

    fixed score!! and added some center stuff

--- src/main/java/frc/robot/subsystems/NetworkTables.java
+++ src/main/java/frc/robot/subsystems/NetworkTables.java
@@ -5,7 +5,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class NetworkTables {
 
-    private static double[] arr = {0,0};
+    private static double[] arr = {-2169,-2169};
     private static NetworkTable table = NetworkTableInstance.getDefault().getTable("SmartDashboard");
     public NetworkTables() {}
 
@@ -15,7 +15,7 @@ public class NetworkTables {
     }
 
     public static double getPalmAngle(String coneOrCube) {
-        return table.getEntry("Palm-" + coneOrCube + "-Angle").getDouble(-1);
+        return table.getEntry("Palm-" + coneOrCube + "-Angle").getDouble(-2169);
     }
 
     public static double[] getFrontCenter(String coneOrCube) {
@@ -23,12 +23,12 @@ public class NetworkTables {
     }
 
     public static double[] getFrontCenter() {
-        return getFrontCenter("Cone")[0] == 0 ? 
+        return getFrontCenter("Cone")[0] == -2169 ? 
         getFrontCenter("Cube") : getFrontCenter("Cone");
     }
 
     public static double[] closestObject() {
-        if(getFrontCenter("Cube")[1] != 0 || getFrontCenter("Cone")[1] != 0) {
+        if(getFrontCenter("Cube")[1] != -2169 || getFrontCenter("Cone")[1] != -2169) {
             return table.getEntry("front-Cone-Center").getDoubleArray(arr)[1] < table.getEntry("front-Cone-Center").getDoubleArray(arr)[1] ? 
         getFrontCenter("Cone") : getFrontCenter("Cube");
         }
@@ -37,11 +37,11 @@ public class NetworkTables {
     }
 
     public static String isThereObject() {
-        if(getFrontCenter("Cone")[0] != 0 && getFrontCenter("Cube")[0] != 0)
+        if(getFrontCenter("Cone")[0] != -2169 && getFrontCenter("Cube")[0] != -2169)
             return "Both";
-        else if(getFrontCenter("Cube")[0] != 0) 
+        else if(getFrontCenter("Cube")[0] != -2169) 
             return "Cube";
-        else if(getFrontCenter("Cone")[0] != 0)
+        else if(getFrontCenter("Cone")[0] != -2169)
             return "Cone";
         else
             return "None";
@@ -59,42 +59,50 @@ public class NetworkTables {
     }
 
     public static double frontApriltagYaw() {
-        return table.getEntry("front-apriltag-Yaw").getDouble(-1);
+        return table.getEntry("front-apriltag-Yaw").getDouble(-2169);
     }
     public static long frontApriltagId() {
-        return table.getEntry("front-apriltag-Id").getInteger(-1);
+        return table.getEntry("front-apriltag-Id").getInteger(-2169);
     }
     public static double frontApriltagX() {
-        return table.getEntry("front-apriltag-X").getDouble(-1);
+        return table.getEntry("front-apriltag-X").getDouble(-2169);
     }
     public static double leftApriltagYaw() {
-        return table.getEntry("Left-apriltag-Yaw").getDouble(-1);
+        return table.getEntry("Left-apriltag-Yaw").getDouble(-2169);
     }
     public static long leftApriltagId() {
-        return table.getEntry("Left-apriltag-Id").getInteger(-1);
+        return table.getEntry("Left-apriltag-Id").getInteger(-2169);
     }
     public static double leftApriltagX() {
-        return table.getEntry("Left-apriltag-X").getDouble(-1);
+        return table.getEntry("Left-apriltag-X").getDouble(-2169);
     }
     public static double rightApriltagYaw() {
-        return table.getEntry("Right-apriltag-Yaw").getDouble(-1);
+        return table.getEntry("Right-apriltag-Yaw").getDouble(-2169);
     }
     public static long rightApriltagId() {
-        return table.getEntry("Right-apriltag-Id").getInteger(-1);
+        return table.getEntry("Right-apriltag-Id").getInteger(-2169);
     }
     public static double rightApriltagX() {
-        return table.getEntry("Right-apriltag-X").getDouble(-1);
+        return table.getEntry("Right-apriltag-X").getDouble(-2169);
     }
     public static double rightApriltagY() {
-        return table.getEntry("Right-apriltag-Y").getDouble(-1);
+        return table.getEntry("Right-apriltag-Y").getDouble(-2169);
     }
     public static double leftApriltagY() {
-        return table.getEntry("Left-apriltag-Y").getDouble(-1);
+        return table.getEntry("Left-apriltag-Y").getDouble(-2169);
     }
     public static double frontApriltagY() {
-        return table.getEntry("Front-apriltag-Y").getDouble(-1);
+        return table.getEntry("Front-apriltag-Y").getDouble(-2169);
     }
-    
+    public static double[] leftApriltagCenter() {
+        return table.getEntry("Left-apriltag-Center").getDoubleArray(arr);
+    }
+    public static double[] rightApriltagCenter() {
+        return table.getEntry("Right-apriltag-Center").getDoubleArray(arr);
+    }
+    public static double[] FrontApriltagCenter() {
+        return table.getEntry("Front-apriltag-Center").getDoubleArray(arr);
+    }
 
         
 }
